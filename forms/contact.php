cf206d98bb80498550888ac5794c7607472a4445
Takudzwa Chikasha <harrisonchikks@gmail.com>
2023-08-30 19:26:35 +0200

    Second Commit Three

--- forms/contact.php
+++ forms/contact.php
@@ -7,21 +7,24 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+ if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    
+    $to = "ops@natureaviation.com, natureaviation@gmail.com";
+    $subject = "New Contact Form Submission";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    
+    $message_body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    
+    if (mail($to, $subject, $message_body, $headers)) {
+        $success_message = "Your message has been sent successfully!";
+    } else {
+        $error_message = "Oops! Something went wrong and we couldn't send your message.";
+    }
+}
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,9 +36,6 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
 ?>
