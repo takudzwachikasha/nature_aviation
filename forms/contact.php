f3e6654513053d08d16fee4521a37e020c02dce2
Takudzwa Chikasha <harrisonchikks@gmail.com>
2023-09-19 12:39:59 +0200

    More

--- forms/contact.php
+++ forms/contact.php
@@ -1,32 +1,33 @@
-
 <?php
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the form fields and sanitize user input
-    $name = ($_POST["name"]);
-    $email = ($_POST["email"]);
-    $subject =($_POST["subject"]);
-    $message =($_POST["message"]);
+// Check if the form was submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanitize and validate user input
+    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    $subject = filter_input(INPUT_POST, "subject", FILTER_SANITIZE_STRING);
+    $message = filter_input(INPUT_POST, "message", FILTER_SANITIZE_STRING);
 
-    // Set your email address where you want to receive messages
-    $to = "takudzwa17@live.com";
+    // Check if required fields are not empty
+    if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+        echo "Please fill in all required fields.";
+    } else {
+        // Set your email address where you want to receive messages
+        $to = "harrisonchikks@gmail.com";
 
-    // Create email headers
-    $headers = "From: $name <$email>\r\n";
-    //$headers .= "Reply-To: $email\r\n";
+        // Create email headers
+        $headers = "From: $name <$email>\r\n";
+        $headers .= "Reply-To: $email\r\n";
 
-    // Send the email
-    mail($to, $subject, $message, $headers) 
-    or die(
-        'Error'
-    );
-
-    echo"message send!";
-  //  } else {
-  //      echo "error";
- //   }
-// } else {
-//     // Not a POST request, so redirect to the contact form page
-//     header("Location: index.html"); // Replace with the actual filename of your form page
-//     exit();
-// }
+        // Send the email
+        if (mail($to, $subject, $message, $headers)) {
+            echo "Message sent successfully!";
+        } else {
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
+    }
+} else {
+    // Redirect to the contact form page if accessed directly
+    header("Location: index.html"); // Replace with the actual filename of your form page
+    exit();
+}
 ?>
